8763fba702cd5640216e501d221e3e66b6251229
Syed Izhan Hassan <163643610+IzhanHassan007@users.noreply.github.com>
2025-02-16 11:00:35 +0000

    container config done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,14 @@
+const accountId = 133224
+let accountEmail = "izhan@google.com"
+var accountPassword = "122456"
+accountCity = "Karachi"
+
+// accountId = 2    // not allowed
+
+accountEmail = "heasd@gmai.com"
+accountPassword = "134325"
+accountCity = "Lahore"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
